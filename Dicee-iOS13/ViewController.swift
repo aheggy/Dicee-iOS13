0e19621e6b1ca29b1ad840c034b212a911cd716f
Ahmed Heggy <ahmedheggy@pursuit.org>
2024-10-21 11:34:25 -0400

    add the image view for two dice, logo and  button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // WHO         WHAT    VALUE
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        
+        //diceImageView1.alpha = 0.5
+        
+        }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(named: "DiceFour")
+        diceImageView2.image = UIImage(named: "DiceFour")
+
     }
-
-
+    
 }
 
