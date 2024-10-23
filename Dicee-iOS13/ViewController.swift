e4ec4418f4a42ce49033e5b776b3fea2bc1ceb69
Ahmed Heggy <ahmedheggy@pursuit.org>
2024-10-23 13:04:57 -0400

    added functionality for the roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    var diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // WHO         WHAT    VALUE
-        diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView2.image = UIImage(named: "DiceTwo")
+        diceImageView1.image = UIImage(named: "\(diceArray[0])")
+        diceImageView2.image = UIImage(named: "\(diceArray[5])")
         
-        //diceImageView1.alpha = 0.5
+//        diceImageView1.alpha = 0.5
         
         }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(named: "DiceFour")
-        diceImageView2.image = UIImage(named: "DiceFour")
-
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+        
+        diceImageView1.image = UIImage(named: "\(diceArray[leftDiceNumber])")
+        diceImageView2.image = UIImage(named: "\(diceArray[rightDiceNumber])")
+        
+        
     }
     
 }
